f21aee9240b54be97f4ab0a367c86be2003a9057
Vikash Yadav <v-viyada@microsoft.com>
2024-02-16 20:58:06 -0500

    Added license header

--- dev/website-hashroute/src/Linked1/Linked1.js
+++ dev/website-hashroute/src/Linked1/Linked1.js
@@ -1,14 +1,21 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Linked1 = () => {
-  return (
-    <div>
-        <h1>Linked page with inner page</h1>
-        <p><NavLink to="/home">Back to home</NavLink></p>
-        <p><NavLink to="/innerpage">To inner page</NavLink></p>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Linked page with inner page</h1>
+            <p>
+                <NavLink to="/home">Back to home</NavLink>
+            </p>
+            <p>
+                <NavLink to="/innerpage">To inner page</NavLink>
+            </p>
+        </div>
+    );
 };
 
 export default Linked1;
